69367487b152609cffc1888e37369a0edd558a6a
sazax1 <84601300+sazax1@users.noreply.github.com>
2022-07-13 21:42:42 -0300

    Update do_move.m

--- do_move.m
+++ do_move.m
@@ -1,19 +1,14 @@
 % Universidade Federal de Mato Grosso
-% Instituto de Engenharia
 %
 % Funcao Octave/MATLAB que implementa o modelo de transicao do
 % quebra-cabeca de oito pecas.
 % A funcao recebe uma matriz __M__ que representa um estado do jogo 
 % e uma pecaa __p_value__ a ser movimentada e devolve uma nova matriz 
 % __nM__ com o estado do jogo apos a movimentacao.
-%
 % Uma execucao da funcao para 
-%
 % S = [4 1 3; 9 2 5; 7 8 6];
 % p_value = 2;
-%
 % por exemplo, devolve o estado:
-%
 % [4 1 3; 2 9 5; 7 8 6]; 
 
 
